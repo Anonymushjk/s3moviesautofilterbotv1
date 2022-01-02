510aa698d75dfeca44cddd69b1a3381ef6bcc3c1
Anonymushjk <94300623+Anonymushjk@users.noreply.github.com>
2022-01-02 16:13:36 +0530

    Update sample_info.py

--- sample_info.py
+++ sample_info.py
@@ -1,10 +1,10 @@
 # Bot information
-SESSION = 'LuciferMoringstar_Robot'
+SESSION = 'S3Moviesfilterbot'
 USER_SESSION = 'User_Bot'
 API_ID = 12345
 API_HASH = '0123456789abcdef0123456789abcdef'
 BOT_TOKEN = '123456:LuciferMoringstar_Robot-zyx57W2v1u123ew11'
-USERBOT_STRING_SESSION = 'LuciferMoringstar_Robot'
+USERBOT_STRING_SESSION = 'S3Moviesfilterbot'
 
 # Bot settings
 CACHE_TIME = 300
@@ -12,13 +12,13 @@ USE_CAPTION_FILTER = False
 
 # Admins, Channels & Users
 ADMINS = [12345789, 'admin123', 987654321]
-CHANNELS = [-10012345678, -100987654321, 'Mo_Tech_YT']
+CHANNELS = [-10012345678, -100987654321, 'S³Movies Channel™️']
 AUTH_USERS = []
 AUTH_CHANNEL = None
 
 # MongoDB information
-DATABASE_URI = "mongodb://[LuciferMoringstar_Robot:LuciferMoringstar_Robot@]host1[:port1][,...hostN[:portN]][/[defaultauthdb]?retryWrites=true&w=majority"
-DATABASE_NAME = 'LuciferMoringstar_Robot'
+DATABASE_URI = "mongodb://[S3Moviesfilterbot:S3Moviesfilterbot@]host1[:port1][,...hostN[:portN]][/[defaultauthdb]?retryWrites=true&w=majority"
+DATABASE_NAME = 'S3Moviesfilterbot'
 COLLECTION_NAME = 'channel_files'  # If you are using the same database, then use different collection name for each bot
 
 # Messages
