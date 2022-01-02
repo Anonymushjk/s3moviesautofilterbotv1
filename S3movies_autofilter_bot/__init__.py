6d1a358fc2992a1859bb21df96fc836d31d03dc2
Anonymushjk <94300623+Anonymushjk@users.noreply.github.com>
2022-01-02 16:20:58 +0530

    Update __init__.py

--- S3movies_autofilter_bot/__init__.py
+++ S3movies_autofilter_bot/__init__.py
@@ -28,14 +28,9 @@ Bot Owner Only
 """
 
 ABOUT = """
-➥ Name :  Auto Filter V3
-    
-➥ Creator : [Naughty PROFESSOR](
-https://youtube.com/c/NaughtyPROFESSOR)
+➥ Name :  S3Moviesfilterbot
 
 ➥ Language : Python3
 
 ➥ Library : Pyrogram Asyncio 1.13.0
-
-➥ Tutorial : [Click Me](https://youtu.be/5hnYOKBzyi8)
 """
